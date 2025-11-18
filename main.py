9a20ea5e5ce520bd0f3c6359548653d13bb9ceea
Pavel <piv17032009@gmail.com>
2025-11-18 19:05:26 +0300

    .

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import requests
 import os
 import argparse
 from dotenv import load_dotenv
-load_dotenv()
 
 
-API_KEY=os.getenv("API_KEY")
-
-
-def get_conversion_rates(currency):
-    url=f"https://v6.exchangerate-api.com/v6/{API_KEY}/latest/RUB"
+def get_conversion_rates(currency, api_key):
+    url=f"https://v6.exchangerate-api.com/v6/{api_key}/latest/{currency.upper()}"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()["conversion_rates"]
@@ -22,15 +18,19 @@ def get_target_money(base_money, target_currency, conversion_rates):
 
 
 def main():
+    load_dotenv()
+    api_key=os.getenv("API_KEY")
     parser = argparse.ArgumentParser(description=" Эта программа переводит деньги на другую валюту")
     parser.add_argument("-b", "--base", help="Введите код базовой валюты(например rub)", required=True)
     parser.add_argument("-t", "--target", help="Введите код целевой валюты(например eur)", required=True)
     parser.add_argument("-a", "--amount", help="Введите сумму", required=True)
     args = parser.parse_args()
-    print(f"Конвертируемая сумма: {get_target_money(args.amount, args.target, get_conversion_rates(args.base))} {args.target.upper()}")
+    try:
+        print(f"Конвертируемая сумма: {get_target_money(args.amount, args.target, get_conversion_rates(args.base, api_key))} {args.target.upper()}")
+    except HTTPError:
+        print("Вы допустили ошибку в запросе")
+    
 
 if __name__ == "__main__":
-    try:
-        main()
-    except HTTPError:
-        print("Вы доаустили ошибку в запросе")
+    main()
+    
